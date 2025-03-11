afd589d7a8c27d25747b753b3a1b5e4803cff8f1
longmaker2 <l.deng@alustudent.com>
2025-03-12 02:34:40 +0300

    updated the vite config

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,7 +1,3 @@
-import dotenv from "dotenv";
-dotenv.config();
-
 export const STRIPE_PUBLISHABLE_KEY =
-  process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY || // For CRA
-  import.meta.env?.VITE_STRIPE_PUBLISHABLE_KEY || // For Vite
+  import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY ||
   "pk_test_51R1V6q07gKx24rpiZjXRcf84SdMraXj90Ze5W8ScYyWfs6GrimmkipURPoNoinDrVC7gu4oLnobwxlPMPXBvwOJz00iMMbS7qK";
